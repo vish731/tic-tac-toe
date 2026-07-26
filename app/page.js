15d0b501373e19a2c1e17fcce3fcf9684adf2a02
Gojo <vishalvc16vc@gmail.com>
2026-07-26 23:02:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+import { Onboarding } from '@/components/Onboarding';
+import { TicTacToeBoard } from '@/components/TicTacToeBoard';
+
+export default function Home() {
+  const [started, setStarted] = useState(false);
+
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      {started ? <TicTacToeBoard /> : <Onboarding onStart={() => setStarted(true)} />}
+    </main>
+  );
+}
 
