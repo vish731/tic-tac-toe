f2e51625fd53acb188d2e72efa0c0b4ded889f42
Gojo <vishalvc16vc@gmail.com>
2026-07-27 17:23:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const webpack = require('webpack');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,8 +7,9 @@ const nextConfig = {
   // feature). Those packages aren't installed, so webpack fails to
   // resolve them. IgnorePlugin tells webpack to skip ANY import
   // matching @x402/... instead of erroring out, whichever submodule
-  // it happens to be.
-  webpack: (config) => {
+  // it happens to be. (We also removed the baseAccount connector
+  // itself in lib/wallet.js, but this stays as extra insurance.)
+  webpack: (config, { webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^@x402\// }));
     return config;
   },
