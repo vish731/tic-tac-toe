582cf2b3abc54178909d61a3cb2b12d9d363809e
Gojo <vishalvc16vc@gmail.com>
2026-07-27 17:18:19 +0530

    Configure webpack to ignore @x402/* imports
    Added IgnorePlugin to webpack configuration to skip imports from @x402/*.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // The baseAccount wagmi connector pulls in @coinbase/cdp-sdk, which
+  // statically imports optional @x402/* payment modules we don't use
+  // (we only need basic wallet connect, not Base's x402 payments
+  // feature). Those packages aren't installed, so webpack fails to
+  // resolve them. IgnorePlugin tells webpack to skip ANY import
+  // matching @x402/... instead of erroring out, whichever submodule
+  // it happens to be.
+  webpack: (config) => {
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^@x402\// }));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
